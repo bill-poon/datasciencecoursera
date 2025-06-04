685c4ff54e8859b24653489a83f8ecc9626e91db
bill-poon <106442904+bill-poon@users.noreply.github.com>
2025-06-04 08:43:26 -0600

    run_analysis.R

---
+++ run_analysis.R
@@ -0,0 +1,45 @@
+library(dyplr)
+
+filename <- "dataset.zip"
+if (!file.exists(filename)) {
+  fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+  download.file(fileURL, filename, method = "curl")
+}
+if (!file.exists("UCI HAR Dataset")) {
+  unzip(filename)
+}
+
+features <- read.table("UCI HAR Dataset/features.txt", col.names = c("index", "feature"))
+activities <- read.table("UCI HAR Dataset/activity_labels.txt", col.names = c("code", "activity"))
+
+subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt", col.names = "subject")
+x_train <- read.table("UCI HAR Dataset/train/X_train.txt", col.names = features$feature)
+y_train <- read.table("UCI HAR Dataset/train/y_train.txt", col.names = "code")
+
+subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt", col.names = "subject")
+x_test <- read.table("UCI HAR Dataset/test/X_test.txt", col.names = features$feature)
+y_test <- read.table("UCI HAR Dataset/test/y_test.txt", col.names = "code")
+
+X <- rbind(x_train, x_test)
+Y <- rbind(y_train, y_test)
+Subject <- rbind(subject_train, subject_test)
+merged_data <- cbind(Subject, Y, X)
+
+tidy_data <- merged_data %>%
+  select(subject, code, contains("mean"), contains("std"))
+
+tidy_data$code <- activities[tidy_data$code, 2]
+names(tidy_data)[2] <- "activity"
+
+names(tidy_data) <- gsub("^t", "Time", names(tidy_data))
+names(tidy_data) <- gsub("^f", "Frequency", names(tidy_data))
+names(tidy_data) <- gsub("Acc", "Accelerometer", names(tidy_data))
+names(tidy_data) <- gsub("Gyro", "Gyroscope", names(tidy_data))
+names(tidy_data) <- gsub("Mag", "Magnitude", names(tidy_data))
+names(tidy_data) <- gsub("BodyBody", "Body", names(tidy_data))
+
+final_data <- tidy_data %>%
+  group_by(subject, activity) %>%
+  summarise_all(mean)
+
+write.table(final_data, "tidy_dataset.txt", row.name = FALSE)
